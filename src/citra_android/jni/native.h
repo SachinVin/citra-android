44721b45193c4873ba1628a5769602423667e532
bunnei <bunneidev@gmail.com>
2019-07-11 18:12:34 -0400

    android: native: Add hooks for SwitchScreenLayout and SwapScreens.

--- src/citra_android/jni/native.h
+++ src/citra_android/jni/native.h
@@ -139,6 +139,12 @@ JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_WriteProfileR
 JNIEXPORT void JNICALL
 Java_org_citra_citra_1android_NativeLibrary_CacheClassesAndMethods(JNIEnv* env, jobject obj);
 
+JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SwitchScreenLayout(JNIEnv* env,
+                                                                                      jobject obj);
+
+JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SwapScreens(JNIEnv* env,
+                                                                               jobject obj);
+
 JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_Run__Ljava_lang_String_2(
     JNIEnv* env, jclass type, jstring path_);
 
