0d246fed224f45bf8305e8e1ddae9e370b630db7
bunnei <bunneidev@gmail.com>
2019-07-18 22:39:02 -0400

    android: frontend: auto-reformat all code for consistent style.

--- src/android/app/src/main/java/org/citra/citra_android/model/settings/Setting.java
+++ src/android/app/src/main/java/org/citra/citra_android/model/settings/Setting.java
@@ -6,52 +6,47 @@ package org.citra.citra_android.model.settings;
  * must be inferred at read-time. The type of value determines which child of this class is used
  * to represent the Setting.
  */
-public abstract class Setting
-{
-  private String mKey;
-  private String mSection;
-  private int mFile;
+public abstract class Setting {
+    private String mKey;
+    private String mSection;
+    private int mFile;
 
-  /**
-   * Base constructor.
-   *
-   * @param key     Everything to the left of the = in a line from the ini file.
-   * @param section The corresponding recent section header; e.g. [Core] or [Enhancements] without the brackets.
-   * @param file    The ini file the Setting is stored in.
-   */
-  public Setting(String key, String section, int file)
-  {
-    mKey = key;
-    mSection = section;
-    mFile = file;
-  }
+    /**
+     * Base constructor.
+     *
+     * @param key     Everything to the left of the = in a line from the ini file.
+     * @param section The corresponding recent section header; e.g. [Core] or [Enhancements] without the brackets.
+     * @param file    The ini file the Setting is stored in.
+     */
+    public Setting(String key, String section, int file) {
+        mKey = key;
+        mSection = section;
+        mFile = file;
+    }
 
-  /**
-   * @return The identifier used to write this setting to the ini file.
-   */
-  public String getKey()
-  {
-    return mKey;
-  }
+    /**
+     * @return The identifier used to write this setting to the ini file.
+     */
+    public String getKey() {
+        return mKey;
+    }
 
-  /**
-   * @return The name of the header under which this Setting should be written in the ini file.
-   */
-  public String getSection()
-  {
-    return mSection;
-  }
+    /**
+     * @return The name of the header under which this Setting should be written in the ini file.
+     */
+    public String getSection() {
+        return mSection;
+    }
 
-  /**
-   * @return The ini file the Setting is stored in.
-   */
-  public int getFile()
-  {
-    return mFile;
-  }
+    /**
+     * @return The ini file the Setting is stored in.
+     */
+    public int getFile() {
+        return mFile;
+    }
 
-  /**
-   * @return A representation of this Setting's backing value converted to a String (e.g. for serialization).
-   */
-  public abstract String getValueAsString();
+    /**
+     * @return A representation of this Setting's backing value converted to a String (e.g. for serialization).
+     */
+    public abstract String getValueAsString();
 }
--- src/android/app/src/main/java/org/citra/citra_android/ui/settings/viewholder/DateTimeViewHolder.java
+++ src/android/app/src/main/java/org/citra/citra_android/ui/settings/viewholder/DateTimeViewHolder.java
@@ -9,21 +9,18 @@ import org.citra.citra_android.model.settings.view.SettingsItem;
 import org.citra.citra_android.ui.settings.SettingsAdapter;
 import org.citra.citra_android.utils.Log;
 
-public final class DateTimeViewHolder extends SettingViewHolder
-{
+public final class DateTimeViewHolder extends SettingViewHolder {
     private DateTimeSetting mItem;
 
     private TextView mTextSettingName;
     private TextView mTextSettingDescription;
 
-    public DateTimeViewHolder(View itemView, SettingsAdapter adapter)
-    {
+    public DateTimeViewHolder(View itemView, SettingsAdapter adapter) {
         super(itemView, adapter);
     }
 
     @Override
-    protected void findViews(View root)
-    {
+    protected void findViews(View root) {
         mTextSettingName = root.findViewById(R.id.text_setting_name);
         Log.error("test " + mTextSettingName);
         mTextSettingDescription = root.findViewById(R.id.text_setting_description);
@@ -31,19 +28,16 @@ public final class DateTimeViewHolder extends SettingViewHolder
     }
 
     @Override
-    public void bind(SettingsItem item)
-    {
+    public void bind(SettingsItem item) {
         mItem = (DateTimeSetting) item;
         mTextSettingName.setText(item.getNameId());
-        if (item.getDescriptionId() > 0)
-        {
+        if (item.getDescriptionId() > 0) {
             mTextSettingDescription.setText(item.getDescriptionId());
         }
     }
 
     @Override
-    public void onClick(View clicked)
-    {
+    public void onClick(View clicked) {
         getAdapter().onDateTimeClick(mItem);
     }
 }
