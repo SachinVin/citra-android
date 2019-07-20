7a5e441136760e8fc8e03bdf1d4893d43e0b0cd4
bunnei <bunneidev@gmail.com>
2019-07-20 04:06:24 -0400

    android: frontend: StartupHandler: Add an intro sequence to pick game dir.

--- src/android/app/src/main/java/org/citra/citra_android/utils/PermissionsHandler.java
+++ src/android/app/src/main/java/org/citra/citra_android/utils/PermissionsHandler.java
@@ -17,15 +17,18 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 public class PermissionsHandler {
     public static final int REQUEST_CODE_WRITE_PERMISSION = 500;
 
+    // We use permissions acceptance as an indicator if this is a first boot for the user.
+    public static boolean isFirstBoot(final FragmentActivity activity) {
+        return ContextCompat.checkSelfPermission(activity, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED;
+    }
+
     @TargetApi(Build.VERSION_CODES.M)
     public static boolean checkWritePermission(final FragmentActivity activity) {
         if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             return true;
         }
 
-        int hasWritePermission = ContextCompat.checkSelfPermission(activity, WRITE_EXTERNAL_STORAGE);
-
-        if (hasWritePermission != PackageManager.PERMISSION_GRANTED) {
+        if (isFirstBoot(activity)) {
             if (activity.shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) {
                 showMessageOKCancel(activity, activity.getString(R.string.write_permission_needed),
                         (dialog, which) -> activity.requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE},
@@ -53,6 +56,7 @@ public class PermissionsHandler {
     private static void showMessageOKCancel(final FragmentActivity activity, String message,
                                             DialogInterface.OnClickListener okListener) {
         new AlertDialog.Builder(activity)
+                .setIcon(R.drawable.ic_citra)
                 .setMessage(message)
                 .setPositiveButton(android.R.string.ok, okListener)
                 .setNegativeButton(android.R.string.cancel, (dialogInterface, i) ->
