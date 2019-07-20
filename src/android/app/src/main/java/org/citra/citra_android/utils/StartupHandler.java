832314a813960238f23ef150a06ba6d93bfb1cc5
bunnei <bunneidev@gmail.com>
2019-07-20 04:06:24 -0400

    android: frontend: StartupHandler: Add an intro sequence to pick game dir.

--- src/android/app/src/main/java/org/citra/citra_android/utils/StartupHandler.java
+++ src/android/app/src/main/java/org/citra/citra_android/utils/StartupHandler.java
@@ -1,21 +1,26 @@
 package org.citra.citra_android.utils;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
+import android.view.View;
 
+import org.citra.citra_android.R;
 import org.citra.citra_android.activities.EmulationActivity;
 
 public final class StartupHandler {
-    public static void HandleInit(FragmentActivity parent) {
+    private static void handlePermissionsCheck(FragmentActivity parent) {
         // Ask the user to grant write permission if it's not already granted
         PermissionsHandler.checkWritePermission(parent);
 
         String start_file = "";
         Bundle extras = parent.getIntent().getExtras();
-        if (extras != null)
+        if (extras != null) {
             start_file = extras.getString("AutoStartFile");
+        }
 
         if (!TextUtils.isEmpty(start_file)) {
             // Start the emulation activity, send the ISO passed in and finish the main activity
@@ -23,6 +28,30 @@ public final class StartupHandler {
             emulation_intent.putExtra("SelectedGame", start_file);
             parent.startActivity(emulation_intent);
             parent.finish();
+        }
+    }
+
+    public static void HandleInit(FragmentActivity parent) {
+        if (PermissionsHandler.isFirstBoot(parent)) {
+            // Prompt user with standard first boot disclaimer
+            AlertDialog.Builder builder = new AlertDialog.Builder(parent);
+            builder.setTitle(R.string.app_name);
+            builder.setIcon(R.drawable.ic_citra);
+            builder.setMessage(parent.getResources().getString(R.string.app_disclaimer));
+            builder.setPositiveButton("OK", null);
+
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                public void onDismiss(DialogInterface dialog) {
+                    // Ensure user agrees to any necessary app permissions
+                    handlePermissionsCheck(parent);
+
+                    // Immediately prompt user to select a game directory on first boot
+                    View view = parent.findViewById(R.id.button_add_directory);
+                    view.callOnClick();
+                }
+            });
+
+            builder.show();
         }
     }
 }
