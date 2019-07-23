cafb321f10bedfb2d6e9c4aefbe6f32a6724506c
bunnei <bunneidev@gmail.com>
2019-07-23 18:31:44 -0400

    android: frontend: settings: Add performance warnings for relevant settings.

--- src/android/app/src/main/java/org/citra/citra_android/ui/settings/SettingsActivityView.java
+++ src/android/app/src/main/java/org/citra/citra_android/ui/settings/SettingsActivityView.java
@@ -55,8 +55,9 @@ public interface SettingsActivityView {
      * Display a popup text message on screen.
      *
      * @param message The contents of the onscreen message.
+     * @param is_long Whether this should be a long Toast or short one.
      */
-    void showToastMessage(String message);
+    void showToastMessage(String message, boolean is_long);
 
     /**
      * Show the previous fragment.
