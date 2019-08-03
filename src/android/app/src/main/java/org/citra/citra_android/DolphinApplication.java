6505b6dafd1d34eaaf0878000bb5fa82d2f9d0aa
bunnei <bunneidev@gmail.com>
2019-08-03 01:03:17 -0400

    android: app: Add method to get global context.

--- src/android/app/src/main/java/org/citra/citra_android/DolphinApplication.java
+++ src/android/app/src/main/java/org/citra/citra_android/DolphinApplication.java
@@ -1,6 +1,7 @@
 package org.citra.citra_android;
 
 import android.app.Application;
+import android.content.Context;
 
 import org.citra.citra_android.model.GameDatabase;
 import org.citra.citra_android.services.DirectoryInitializationService;
@@ -8,14 +9,21 @@ import org.citra.citra_android.utils.PermissionsHandler;
 
 public class DolphinApplication extends Application {
     public static GameDatabase databaseHelper;
+    private static DolphinApplication application;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        application = this;
 
         if (PermissionsHandler.hasWriteAccess(getApplicationContext()))
             DirectoryInitializationService.startService(getApplicationContext());
 
         databaseHelper = new GameDatabase(this);
+    }
+
+    public static Context getAppContext()
+    {
+        return application.getApplicationContext();
     }
 }
