f5c9d06621610d0f0c37b15fa1e717ac164834af
bunnei <bunneidev@gmail.com>
2019-08-24 15:50:19 -0400

    android: jni: Add IDCache to cache Java methods.

---
+++ src/android/app/src/main/jni/id_cache.h
@@ -0,0 +1,15 @@
+// Copyright 2019 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <jni.h>
+
+namespace IDCache {
+
+JNIEnv* GetEnvForThread();
+jclass GetNativeLibraryClass();
+jmethodID GetDisplayAlertMsg();
+
+} // namespace IDCache
