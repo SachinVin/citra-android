504172121991fa36fb6d0553a53971f8d2a924f3
bunnei <bunneidev@gmail.com>
2019-08-30 22:52:06 -0400

    android: Rename main entry class to CitraApplication.

--- src/android/app/src/main/java/org/citra/citra_android/CitraApplication.java
+++ src/android/app/src/main/java/org/citra/citra_android/CitraApplication.java
@@ -1,3 +1,7 @@
+// Copyright 2019 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
 package org.citra.citra_android;
 
 import android.app.Application;
@@ -7,9 +11,9 @@ import org.citra.citra_android.model.GameDatabase;
 import org.citra.citra_android.services.DirectoryInitializationService;
 import org.citra.citra_android.utils.PermissionsHandler;
 
-public class DolphinApplication extends Application {
+public class CitraApplication extends Application {
     public static GameDatabase databaseHelper;
-    private static DolphinApplication application;
+    private static CitraApplication application;
 
     @Override
     public void onCreate() {
