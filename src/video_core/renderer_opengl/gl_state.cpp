e25abef70ef988de9f40ab2a0ea2840ff223e3e0
bunnei <bunneidev@gmail.com>
2019-09-08 02:00:49 -0400

    android: renderer_opengl: Partially implement glLogicOp on GLES.

--- src/video_core/renderer_opengl/gl_state.cpp
+++ src/video_core/renderer_opengl/gl_state.cpp
@@ -169,10 +169,17 @@ void OpenGLState::Apply() const {
     if (blend.enabled != cur_state.blend.enabled) {
         if (blend.enabled) {
             glEnable(GL_BLEND);
-            glDisable(GL_COLOR_LOGIC_OP);
         } else {
             glDisable(GL_BLEND);
-            glEnable(GL_COLOR_LOGIC_OP);
+        }
+
+        // GLES does not support glLogicOp
+        if (!GLES) {
+            if (blend.enabled) {
+                glDisable(GL_COLOR_LOGIC_OP);
+            } else {
+                glEnable(GL_COLOR_LOGIC_OP);
+            }
         }
     }
 
@@ -196,13 +203,11 @@ void OpenGLState::Apply() const {
         glBlendEquationSeparate(blend.rgb_equation, blend.a_equation);
     }
 
-    // GLES3 does not support glLogicOp
+    // GLES does not support glLogicOp
     if (!GLES) {
         if (logic_op != cur_state.logic_op) {
             glLogicOp(logic_op);
         }
-    } else {
-        LOG_TRACE(Render_OpenGL, "glLogicOps are unimplemented...");
     }
 
     // Textures
