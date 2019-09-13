d642d73b2009166b44a87fd6895288d7474c086f
bunnei <bunneidev@gmail.com>
2019-09-12 22:28:12 -0400

    android: jni: Migrate EmuWindow_Android class to EGL.
    - This enables us to use shared contexts more easily.

--- src/android/app/src/main/jni/emu_window/emu_window.h
+++ src/android/app/src/main/jni/emu_window/emu_window.h
@@ -1,30 +1,22 @@
-// Copyright 2016 Citra Emulator Project
+// Copyright 2019 Citra Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
 #pragma once
 
-#include <memory>
-#include <utility>
+#include <vector>
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
 #include "core/frontend/emu_window.h"
-#include "jni/ndk_helper/GLContext.h"
+
+struct ANativeWindow;
 
 class EmuWindow_Android : public Frontend::EmuWindow {
 public:
     EmuWindow_Android(ANativeWindow* surface);
     ~EmuWindow_Android();
-
-    /// Swap buffers to display the next frame
-    void SwapBuffers() override;
-
-    /// Polls window events
-    void PollEvents() override;
-
-    /// Makes the graphics context current for the caller thread
-    void MakeCurrent() override;
-
-    /// Releases the GL context from the caller thread
-    void DoneCurrent() override;
 
     /// Called by the onSurfaceChanges() method to change the surface
     void OnSurfaceChanged(ANativeWindow* surface);
@@ -35,25 +27,27 @@ public:
     /// Handles movement of touch pointer
     void OnTouchMoved(int x, int y);
 
+    void SwapBuffers() override;
+    void PollEvents() override;
+    void MakeCurrent() override;
+    void DoneCurrent() override;
+
 private:
-    /// Called by PollEvents when a key is pressed or released.
-    void OnKeyEvent(int key, u8 state);
-
-    /// Called by PollEvents when the mouse moves.
-    void OnMouseMotion(s32 x, s32 y);
-
-    /// Called by PollEvents when a mouse button is pressed or released
-    void OnMouseButton(u32 button, u8 state, s32 x, s32 y);
-
-    /// Called when any event that may change the surface(Rotation)
     void OnFramebufferSizeChanged();
+    bool CreateWindowSurface();
+    void DestroyWindowSurface();
+    void DestroyContext();
 
-    /// Internal render window
-    ANativeWindow* render_window;
+    ANativeWindow* render_window{};
+    ANativeWindow* host_window{};
 
-    /// Initialise the EGL context associated with the window
-    bool InitDisplay();
+    bool is_shared{};
+    int window_width{};
+    int window_height{};
+    std::vector<int> shared_attribs;
 
-    /// A helper class for handling the EGL context
-    ndk_helper::GLContext* gl_context;
+    EGLConfig config;
+    EGLSurface egl_surface{};
+    EGLContext egl_context{};
+    EGLDisplay egl_display{};
 };
