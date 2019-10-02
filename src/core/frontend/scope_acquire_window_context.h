5f759b0b15bef2b0a873f586da7795fd18120aa8
bunnei <bunneidev@gmail.com>
2019-10-01 23:48:06 -0400

    android: core: frontend: Port yuzu's code for scope acquire window context.

---
+++ src/core/frontend/scope_acquire_window_context.h
@@ -0,0 +1,23 @@
+// Copyright 2019 yuzu Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "common/common_types.h"
+
+namespace Frontend {
+
+class EmuWindow;
+
+/// Helper class to acquire/release window context within a given scope
+class ScopeAcquireWindowContext : NonCopyable {
+public:
+    explicit ScopeAcquireWindowContext(Frontend::EmuWindow& window);
+    ~ScopeAcquireWindowContext();
+
+private:
+    Frontend::EmuWindow& emu_window;
+};
+
+} // namespace Frontend
