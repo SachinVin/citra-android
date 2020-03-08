459072362be71e367c5b9309e4f6167ea82cd60b
bunnei <bunneidev@gmail.com>
2020-03-07 21:07:46 -0500

    android: emu_window: Adapt for use with split presenter thread.

--- src/android/app/src/main/jni/config.cpp
+++ src/android/app/src/main/jni/config.cpp
@@ -110,7 +110,7 @@ void Config::ReadValues() {
     Settings::values.use_shader_jit = sdl2_config->GetBoolean("Renderer", "use_shader_jit", true);
     Settings::values.resolution_factor =
         static_cast<u16>(sdl2_config->GetInteger("Renderer", "resolution_factor", 1));
-    Settings::values.vsync_enabled = sdl2_config->GetBoolean("Renderer", "vsync_enabled", false);
+    Settings::values.use_vsync_new = sdl2_config->GetBoolean("Renderer", "use_vsync_new", true);
     Settings::values.use_frame_limit = sdl2_config->GetBoolean("Renderer", "use_frame_limit", true);
     Settings::values.frame_limit =
         static_cast<u16>(sdl2_config->GetReal("Renderer", "frame_limit", 1.0) * 100.0);
