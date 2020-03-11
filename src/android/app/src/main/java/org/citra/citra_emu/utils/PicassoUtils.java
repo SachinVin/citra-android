011e982b3cfccaf6f6ba3779198284a36c22c3cb
bunnei <bunneidev@gmail.com>
2020-03-10 22:58:55 -0400

    android: Rename app package from citra_android to citra_emu.
    - There is already a conflicting name on the Play Store.

---
+++ src/android/app/src/main/java/org/citra/citra_emu/utils/PicassoUtils.java
@@ -0,0 +1,28 @@
+package org.citra.citra_emu.utils;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import org.citra.citra_emu.R;
+
+public class PicassoUtils {
+    public static void loadGameBanner(ImageView imageView, String gamePath) {
+        Picasso picassoInstance = new Picasso.Builder(imageView.getContext())
+                .addRequestHandler(new GameBannerRequestHandler())
+                .build();
+
+        picassoInstance
+                .load(Uri.parse("iso:/" + gamePath))
+                .noFade()
+                .noPlaceholder()
+                .fit()
+                .centerInside()
+                .config(Bitmap.Config.RGB_565)
+                .error(R.drawable.no_banner)
+                .transform(new PicassoRoundedCornersTransformation())
+                .into(imageView);
+    }
+}
