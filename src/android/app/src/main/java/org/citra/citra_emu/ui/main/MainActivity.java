75d8d53b6d540281c1cc47331c441965a8c422d2
bunnei <bunneidev@gmail.com>
2020-03-10 22:58:55 -0400

    android: Rename app package from citra_android to citra_emu.
    - There is already a conflicting name on the Play Store.

---
+++ src/android/app/src/main/java/org/citra/citra_emu/ui/main/MainActivity.java
@@ -0,0 +1,206 @@
+package org.citra.citra_emu.ui.main;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.citra.citra_emu.R;
+import org.citra.citra_emu.activities.EmulationActivity;
+import org.citra.citra_emu.model.GameProvider;
+import org.citra.citra_emu.services.DirectoryInitializationService;
+import org.citra.citra_emu.ui.platform.PlatformGamesFragment;
+import org.citra.citra_emu.ui.platform.PlatformGamesView;
+import org.citra.citra_emu.ui.settings.SettingsActivity;
+import org.citra.citra_emu.utils.AddDirectoryHelper;
+import org.citra.citra_emu.utils.FileBrowserHelper;
+import org.citra.citra_emu.utils.PermissionsHandler;
+import org.citra.citra_emu.utils.StartupHandler;
+
+/**
+ * The main Activity of the Lollipop style UI. Manages several PlatformGamesFragments, which
+ * individually display a grid of available games for each Fragment, in a tabbed layout.
+ */
+public final class MainActivity extends AppCompatActivity implements MainView {
+    private Toolbar mToolbar;
+    private int mFrameLayoutId;
+    private PlatformGamesFragment mPlatformGamesFragment;
+
+    private MainPresenter mPresenter = new MainPresenter(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViews();
+
+        setSupportActionBar(mToolbar);
+
+        mFrameLayoutId = R.id.games_platform_frame;
+        mPresenter.onCreate();
+
+        if (savedInstanceState == null) {
+            StartupHandler.HandleInit(this);
+            if (PermissionsHandler.hasWriteAccess(this)) {
+                mPlatformGamesFragment = new PlatformGamesFragment();
+                getSupportFragmentManager().beginTransaction().add(mFrameLayoutId, mPlatformGamesFragment)
+                        .commit();
+            }
+        } else {
+            mPlatformGamesFragment = (PlatformGamesFragment) getSupportFragmentManager().getFragment(savedInstanceState, "mPlatformGamesFragment");
+        }
+
+        // Dismiss previous notifications (should not happen unless a crash occurred)
+        EmulationActivity.tryDismissRunningNotification(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getSupportFragmentManager().putFragment(outState, "mPlatformGamesFragment", mPlatformGamesFragment);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPresenter.addDirIfNeeded(new AddDirectoryHelper(this));
+    }
+
+    // TODO: Replace with a ButterKnife injection.
+    private void findViews() {
+        mToolbar = findViewById(R.id.toolbar_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_game_grid, menu);
+        return true;
+    }
+
+    /**
+     * MainView
+     */
+
+    @Override
+    public void setVersionString(String version) {
+        mToolbar.setSubtitle(version);
+    }
+
+    @Override
+    public void refresh() {
+        getContentResolver().insert(GameProvider.URI_REFRESH, null);
+        refreshFragment();
+    }
+
+    @Override
+    public void refreshFragmentScreenshot(int fragmentPosition) {
+        // Invalidate Picasso image so that the new screenshot is animated in.
+        PlatformGamesView fragment = getPlatformGamesView();
+
+        if (fragment != null) {
+            fragment.refreshScreenshotAtPosition(fragmentPosition);
+        }
+    }
+
+    @Override
+    public void launchSettingsActivity(String menuTag) {
+        if (PermissionsHandler.hasWriteAccess(this)) {
+            SettingsActivity.launch(this, menuTag, "");
+        } else {
+            PermissionsHandler.checkWritePermission(this);
+        }
+    }
+
+    @Override
+    public void launchFileListActivity() {
+        FileBrowserHelper.openDirectoryPicker(this, MainPresenter.REQUEST_ADD_DIRECTORY,
+                R.string.select_game_folder);
+    }
+
+    @Override
+    public void showGames(Cursor games) {
+        // no-op. Handled by PlatformGamesFragment.
+    }
+
+    /**
+     * @param requestCode An int describing whether the Activity that is returning did so successfully.
+     * @param resultCode  An int describing what Activity is giving us this callback.
+     * @param result      The information the returning Activity is providing us.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        switch (requestCode) {
+            case MainPresenter.REQUEST_ADD_DIRECTORY:
+                // If the user picked a file, as opposed to just backing out.
+                if (resultCode == MainActivity.RESULT_OK) {
+                    mPresenter.onDirectorySelected(FileBrowserHelper.getSelectedDirectory(result));
+                }
+                break;
+
+            case MainPresenter.REQUEST_EMULATE_GAME:
+                mPresenter.refreshFragmentScreenshot(resultCode);
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PermissionsHandler.REQUEST_CODE_WRITE_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    DirectoryInitializationService.startService(this);
+
+                    mPlatformGamesFragment = new PlatformGamesFragment();
+                    getSupportFragmentManager().beginTransaction().add(mFrameLayoutId, mPlatformGamesFragment)
+                            .commit();
+
+                    // Immediately prompt user to select a game directory on first boot
+                    findViewById(R.id.button_add_directory).callOnClick();
+                } else {
+                    Toast.makeText(this, R.string.write_permission_needed, Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+    }
+
+    /**
+     * Called by the framework whenever any actionbar/toolbar icon is clicked.
+     *
+     * @param item The icon that was clicked on.
+     * @return True if the event was handled, false to bubble it up to the OS.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mPresenter.handleOptionSelection(item.getItemId());
+    }
+
+    private void refreshFragment() {
+        if (mPlatformGamesFragment != null) {
+            mPlatformGamesFragment.refresh();
+        }
+    }
+
+    @Nullable
+    private PlatformGamesView getPlatformGamesView() {
+        return (PlatformGamesView) getSupportFragmentManager().findFragmentById(mFrameLayoutId);
+    }
+
+    @Override
+    protected void onDestroy() {
+        EmulationActivity.tryDismissRunningNotification(this);
+        super.onDestroy();
+    }
+}
--- src/android/app/src/main/jni/native.h
+++ src/android/app/src/main/jni/native.h
@@ -9,164 +9,164 @@
 extern "C" {
 #endif
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_UnPauseEmulation(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_UnPauseEmulation(JNIEnv* env,
                                                                                     jobject obj);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_PauseEmulation(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_PauseEmulation(JNIEnv* env,
                                                                                   jobject obj);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_StopEmulation(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_StopEmulation(JNIEnv* env,
                                                                                  jobject obj);
 
-JNIEXPORT jboolean JNICALL Java_org_citra_citra_1android_NativeLibrary_IsRunning(JNIEnv* env,
+JNIEXPORT jboolean JNICALL Java_org_citra_citra_1emu_NativeLibrary_IsRunning(JNIEnv* env,
                                                                                  jobject obj);
 
-JNIEXPORT jboolean JNICALL Java_org_citra_citra_1android_NativeLibrary_onGamePadEvent(
+JNIEXPORT jboolean JNICALL Java_org_citra_citra_1emu_NativeLibrary_onGamePadEvent(
     JNIEnv* env, jobject obj, jstring jDevice, jint Button, jint Action);
 
-JNIEXPORT jboolean JNICALL Java_org_citra_citra_1android_NativeLibrary_onGamePadMoveEvent(
+JNIEXPORT jboolean JNICALL Java_org_citra_citra_1emu_NativeLibrary_onGamePadMoveEvent(
     JNIEnv* env, jobject obj, jstring jDevice, jint Axis, jfloat x, jfloat y);
 
-JNIEXPORT jboolean JNICALL Java_org_citra_citra_1android_NativeLibrary_onGamePadAxisEvent(
+JNIEXPORT jboolean JNICALL Java_org_citra_citra_1emu_NativeLibrary_onGamePadAxisEvent(
     JNIEnv* env, jobject obj, jstring jDevice, jint axis_id, jfloat axis_val);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_onTouchEvent(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_onTouchEvent(JNIEnv* env,
                                                                                 jobject obj,
                                                                                 jfloat x, jfloat y,
                                                                                 jboolean pressed);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_onTouchMoved(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_onTouchMoved(JNIEnv* env,
                                                                                 jobject obj,
                                                                                 jfloat x, jfloat y);
 
-JNIEXPORT jintArray JNICALL Java_org_citra_citra_1android_NativeLibrary_GetBanner(JNIEnv* env,
+JNIEXPORT jintArray JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetBanner(JNIEnv* env,
                                                                                   jobject obj,
                                                                                   jstring jFile);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetTitle(JNIEnv* env,
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetTitle(JNIEnv* env,
                                                                                jobject obj,
                                                                                jstring jFilename);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetDescription(
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetDescription(
     JNIEnv* env, jobject obj, jstring jFilename);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetGameId(JNIEnv* env,
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetGameId(JNIEnv* env,
                                                                                 jobject obj,
                                                                                 jstring jFilename);
 
-JNIEXPORT jint JNICALL Java_org_citra_citra_1android_NativeLibrary_GetCountry(JNIEnv* env,
+JNIEXPORT jint JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetCountry(JNIEnv* env,
                                                                               jobject obj,
                                                                               jstring jFilename);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetCompany(JNIEnv* env,
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetCompany(JNIEnv* env,
                                                                                  jobject obj,
                                                                                  jstring jFilename);
 
-JNIEXPORT jlong JNICALL Java_org_citra_citra_1android_NativeLibrary_GetFilesize(JNIEnv* env,
+JNIEXPORT jlong JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetFilesize(JNIEnv* env,
                                                                                 jobject obj,
                                                                                 jstring jFilename);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetVersionString(JNIEnv* env,
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetVersionString(JNIEnv* env,
                                                                                        jobject obj);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetGitRevision(JNIEnv* env,
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetGitRevision(JNIEnv* env,
                                                                                      jobject obj);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SaveScreenShot(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SaveScreenShot(JNIEnv* env,
                                                                                   jobject obj);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_eglBindAPI(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_eglBindAPI(JNIEnv* env,
                                                                               jobject obj,
                                                                               jint api);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetConfig(
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetConfig(
     JNIEnv* env, jobject obj, jstring jFile, jstring jSection, jstring jKey, jstring jDefault);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SetConfig(
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SetConfig(
     JNIEnv* env, jobject obj, jstring jFile, jstring jSection, jstring jKey, jstring jValue);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SetFilename(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SetFilename(JNIEnv* env,
                                                                                jobject obj,
                                                                                jstring jFile);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SaveState(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SaveState(JNIEnv* env,
                                                                              jobject obj, jint slot,
                                                                              jboolean wait);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SaveStateAs(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SaveStateAs(JNIEnv* env,
                                                                                jobject obj,
                                                                                jstring path,
                                                                                jboolean wait);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_LoadState(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_LoadState(JNIEnv* env,
                                                                              jobject obj,
                                                                              jint slot);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_LoadStateAs(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_LoadStateAs(JNIEnv* env,
                                                                                jobject obj,
                                                                                jstring path);
 
 JNIEXPORT void JNICALL
-Java_org_citra_citra_1android_services_DirectoryInitializationService_CreateUserDirectories(
+Java_org_citra_citra_1emu_services_DirectoryInitializationService_CreateUserDirectories(
     JNIEnv* env, jobject obj);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SetUserDirectory(
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SetUserDirectory(
     JNIEnv* env, jobject obj, jstring jDirectory);
 
 JNIEXPORT void JNICALL
-Java_org_citra_citra_1android_services_DirectoryInitializationService_SetSysDirectory(
+Java_org_citra_citra_1emu_services_DirectoryInitializationService_SetSysDirectory(
     JNIEnv* env, jclass type, jstring path_);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetUserDirectory(JNIEnv* env,
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetUserDirectory(JNIEnv* env,
                                                                                        jobject obj);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SetSysDirectory(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SetSysDirectory(JNIEnv* env,
                                                                                    jobject obj,
                                                                                    jstring path);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_CreateConfigFile();
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_CreateConfigFile();
 
-JNIEXPORT jint JNICALL Java_org_citra_citra_1android_NativeLibrary_DefaultCPUCore(JNIEnv* env,
+JNIEXPORT jint JNICALL Java_org_citra_citra_1emu_NativeLibrary_DefaultCPUCore(JNIEnv* env,
                                                                                   jobject obj);
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SetProfiling(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SetProfiling(JNIEnv* env,
                                                                                 jobject obj,
                                                                                 jboolean enable);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_WriteProfileResults(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_WriteProfileResults(JNIEnv* env,
                                                                                        jobject obj);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_NotifyOrientationChange(
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_NotifyOrientationChange(
     JNIEnv* env, jobject obj, jint layout_option, jboolean is_portrait_mode);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SwapScreens(
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SwapScreens(
     JNIEnv* env, jobject obj, jboolean swap_screens, jboolean is_portrait_mode);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_Run__Ljava_lang_String_2(
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_Run__Ljava_lang_String_2(
     JNIEnv* env, jclass type, jstring path_);
 
 JNIEXPORT void JNICALL
-Java_org_citra_citra_1android_NativeLibrary_Run__Ljava_lang_String_2Ljava_lang_String_2Z(
+Java_org_citra_citra_1emu_NativeLibrary_Run__Ljava_lang_String_2Ljava_lang_String_2Z(
     JNIEnv* env, jobject obj, jstring jFile, jstring jSavestate, jboolean jDeleteSavestate);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SurfaceChanged(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SurfaceChanged(JNIEnv* env,
                                                                                   jobject obj,
                                                                                   jobject surf);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SurfaceDestroyed(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SurfaceDestroyed(JNIEnv* env,
                                                                                     jobject obj);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_InitGameIni(JNIEnv* env,
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_InitGameIni(JNIEnv* env,
                                                                                jclass type,
                                                                                jstring gameID_);
 
-JNIEXPORT void JNICALL Java_org_citra_citra_1android_NativeLibrary_SetUserSetting(
+JNIEXPORT void JNICALL Java_org_citra_citra_1emu_NativeLibrary_SetUserSetting(
     JNIEnv* env, jclass type, jstring gameID_, jstring Section_, jstring Key_, jstring Value_);
 
-JNIEXPORT jstring JNICALL Java_org_citra_citra_1android_NativeLibrary_GetUserSetting(
+JNIEXPORT jstring JNICALL Java_org_citra_citra_1emu_NativeLibrary_GetUserSetting(
     JNIEnv* env, jclass type, jstring gameID_, jstring Section_, jstring Key_);
 
 JNIEXPORT jdoubleArray JNICALL
-Java_org_citra_citra_1android_NativeLibrary_GetPerfStats(JNIEnv* env, jclass type);
+Java_org_citra_citra_1emu_NativeLibrary_GetPerfStats(JNIEnv* env, jclass type);
 
 #ifdef __cplusplus
 }
