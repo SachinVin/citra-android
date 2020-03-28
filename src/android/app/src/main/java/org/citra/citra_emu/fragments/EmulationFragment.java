a56ccfa03bcb2efe2892f8d5277ce930053b56e6
FearlessTobi <thm.frey@gmail.com>
2020-03-28 20:39:25 +0100

    Android: don't try to pause emulation when not running
    Forcing landscape at emulation start revealed a bug where if the activity was
    recreated before emulation started then it would get stuck in a paused state.
    
    Original commit by zackhow for Dolphin-emu.

--- src/android/app/src/main/java/org/citra/citra_emu/fragments/EmulationFragment.java
+++ src/android/app/src/main/java/org/citra/citra_emu/fragments/EmulationFragment.java
@@ -127,7 +127,8 @@ public final class EmulationFragment extends Fragment implements SurfaceHolder.C
             directoryStateReceiver = null;
         }
 
-        mEmulationState.pause();
+        if (mEmulationState.isRunning())
+            mEmulationState.pause();
         super.onPause();
     }
 
