dcf9f8747f89ad5b7a2f8d1b26d84a9a7e41b99a
FearlessTobi <thm.frey@gmail.com>
2020-03-28 20:14:30 +0100

    Android: Change the file browser dialog ok button title
    I hope this will make it more clear to users that they are suppose to
    select the dircetory that has the games.
    
    Original commit by mahdihijazi for Dolphin-emu.

--- src/android/app/src/main/java/org/citra/citra_emu/fragments/CustomFilePickerFragment.java
+++ src/android/app/src/main/java/org/citra/citra_emu/fragments/CustomFilePickerFragment.java
@@ -1,9 +1,11 @@
 package org.citra.citra_emu.fragments;
 
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +27,25 @@ public class CustomFilePickerFragment extends FilePickerFragment {
                 .getUriForFile(getContext(),
                         getContext().getApplicationContext().getPackageName() + ".filesprovider",
                         file);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        if (view == null)
+            return null;
+
+        TextView ok = view.findViewById(R.id.nnf_button_ok);
+        TextView cancel = view.findViewById(R.id.nnf_button_cancel);
+
+        ok.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        cancel.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+
+        ok.setAllCaps(false);
+        cancel.setAllCaps(false);
+        ok.setText(R.string.select_dir);
+        return view;
     }
 
     @Override
