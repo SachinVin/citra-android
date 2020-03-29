31c702b89b828a31c0c3835d10e2d077c207889a
FearlessTobi <thm.frey@gmail.com>
2020-03-29 18:17:20 +0200

    Migrate to AndroidX
    Support Libraries are outdated and AndroidX is recommended instead. Read more here: developer.android.com/jetpack/androidx.
    
    Original commit by Simonx22 for Dolphin-emu.

--- src/android/app/src/main/java/org/citra/citra_emu/model/GameProvider.java
+++ src/android/app/src/main/java/org/citra/citra_emu/model/GameProvider.java
@@ -5,7 +5,7 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import org.citra.citra_emu.BuildConfig;
 import org.citra.citra_emu.utils.Log;
--- src/android/app/src/main/java/org/citra/citra_emu/utils/StartupHandler.java
+++ src/android/app/src/main/java/org/citra/citra_emu/utils/StartupHandler.java
@@ -3,7 +3,7 @@ package org.citra.citra_emu.utils;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.text.TextUtils;
 
 import org.citra.citra_emu.R;
