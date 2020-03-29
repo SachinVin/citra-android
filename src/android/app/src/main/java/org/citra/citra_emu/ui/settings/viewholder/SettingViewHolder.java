b808196faa20126017668362cce263b51546d07d
FearlessTobi <thm.frey@gmail.com>
2020-03-29 18:17:20 +0200

    Migrate to AndroidX
    Support Libraries are outdated and AndroidX is recommended instead. Read more here: developer.android.com/jetpack/androidx.
    
    Original commit by Simonx22 for Dolphin-emu.

--- src/android/app/src/main/java/org/citra/citra_emu/ui/settings/viewholder/SettingViewHolder.java
+++ src/android/app/src/main/java/org/citra/citra_emu/ui/settings/viewholder/SettingViewHolder.java
@@ -1,6 +1,6 @@
 package org.citra.citra_emu.ui.settings.viewholder;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 
 import org.citra.citra_emu.model.settings.view.SettingsItem;
