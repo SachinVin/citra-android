8a2aba6bd1b6ec7c7b2559871e8f706d1c21204c
BreadFish64 <mohror64@gmail.com>
2020-03-30 09:17:40 -0500

    android: jni cleanup

--- src/android/app/src/main/jni/game_info.cpp
+++ src/android/app/src/main/jni/game_info.cpp
@@ -68,7 +68,7 @@ char16_t* GetTitle(std::string physical_name) {
     char16_t* title;
     title = reinterpret_cast<char16_t*>(smdh.titles[static_cast<int>(language)].long_title.data());
 
-    LOG_INFO(Frontend, "Title: %s", Common::UTF16ToUTF8(title).data());
+    LOG_INFO(Frontend, "Title: {}", Common::UTF16ToUTF8(title));
 
     return title;
 }
@@ -91,7 +91,7 @@ char16_t* GetPublisher(std::string physical_name) {
     publisher =
         reinterpret_cast<char16_t*>(smdh.titles[static_cast<int>(language)].publisher.data());
 
-    LOG_INFO(Frontend, "Publisher: %s", Common::UTF16ToUTF8(publisher).data());
+    LOG_INFO(Frontend, "Publisher: {}", Common::UTF16ToUTF8(publisher));
 
     return publisher;
 }
