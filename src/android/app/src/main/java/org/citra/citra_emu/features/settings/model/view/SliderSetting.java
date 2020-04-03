7cb0065a76cffff1a69d8e84a41009eef4af2eb0
FearlessTobi <thm.frey@gmail.com>
2020-04-03 02:09:47 +0200

    Android: Start structure the project around features instead of data types
    This only moves the settings feature, the rest will be moved slowely later.
    
    Original commit by mahdihijazi for Dolphin-emu.

---
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/model/view/SliderSetting.java
@@ -0,0 +1,110 @@
+package org.citra.citra_emu.features.settings.model.view;
+
+import org.citra.citra_emu.features.settings.model.FloatSetting;
+import org.citra.citra_emu.features.settings.model.IntSetting;
+import org.citra.citra_emu.features.settings.model.Setting;
+import org.citra.citra_emu.utils.Log;
+import org.citra.citra_emu.features.settings.utils.SettingsFile;
+
+public final class SliderSetting extends SettingsItem {
+    private int mMin;
+    private int mMax;
+    private int mDefaultValue;
+
+    private String mUnits;
+
+    public SliderSetting(String key, String section, int file, int titleId, int descriptionId,
+                         int min, int max, String units, int defaultValue, Setting setting) {
+        super(key, section, file, setting, titleId, descriptionId);
+        mMin = min;
+        mMax = max;
+        mUnits = units;
+        mDefaultValue = defaultValue;
+    }
+
+    public int getMin() {
+        return mMin;
+    }
+
+    public int getMax() {
+        return mMax;
+    }
+
+    public int getDefaultValue() {
+        return mDefaultValue;
+    }
+
+    public int getSelectedValue() {
+        Setting setting = getSetting();
+
+        if (setting == null) {
+            return mDefaultValue;
+        }
+
+        if (setting instanceof IntSetting) {
+            IntSetting intSetting = (IntSetting) setting;
+            return intSetting.getValue();
+        } else if (setting instanceof FloatSetting) {
+            FloatSetting floatSetting = (FloatSetting) setting;
+            if (isPercentSetting()) {
+                return Math.round(floatSetting.getValue() * 100);
+            } else {
+                return Math.round(floatSetting.getValue());
+            }
+        } else {
+            Log.error("[SliderSetting] Error casting setting type.");
+            return -1;
+        }
+    }
+
+    public boolean isPercentSetting() {
+        return getKey().equals(SettingsFile.KEY_FRAME_LIMIT);
+    }
+
+    /**
+     * Write a value to the backing int. If that int was previously null,
+     * initializes a new one and returns it, so it can be added to the Hashmap.
+     *
+     * @param selection New value of the int.
+     * @return null if overwritten successfully otherwise; a newly created IntSetting.
+     */
+    public IntSetting setSelectedValue(int selection) {
+        if (getSetting() == null) {
+            IntSetting setting = new IntSetting(getKey(), getSection(), getFile(), selection);
+            setSetting(setting);
+            return setting;
+        } else {
+            IntSetting setting = (IntSetting) getSetting();
+            setting.setValue(selection);
+            return null;
+        }
+    }
+
+    /**
+     * Write a value to the backing float. If that float was previously null,
+     * initializes a new one and returns it, so it can be added to the Hashmap.
+     *
+     * @param selection New value of the float.
+     * @return null if overwritten successfully otherwise; a newly created FloatSetting.
+     */
+    public FloatSetting setSelectedValue(float selection) {
+        if (getSetting() == null) {
+            FloatSetting setting = new FloatSetting(getKey(), getSection(), getFile(), selection);
+            setSetting(setting);
+            return setting;
+        } else {
+            FloatSetting setting = (FloatSetting) getSetting();
+            setting.setValue(selection);
+            return null;
+        }
+    }
+
+    public String getUnits() {
+        return mUnits;
+    }
+
+    @Override
+    public int getType() {
+        return TYPE_SLIDER;
+    }
+}
