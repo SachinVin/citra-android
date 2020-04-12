acfb45f0913f88a7cc0bc981c27ca739e58ee669
FearlessTobi <thm.frey@gmail.com>
2020-04-12 08:56:14 +0200

    android: Fix the menu animations
    Turns out those unused methods were actually used for something.

--- src/android/app/src/main/java/org/citra/citra_emu/features/settings/ui/SettingsFrameLayout.java
+++ src/android/app/src/main/java/org/citra/citra_emu/features/settings/ui/SettingsFrameLayout.java
@@ -6,8 +6,10 @@ import android.widget.FrameLayout;
 
 /**
  * FrameLayout subclass with few Properties added to simplify animations.
+ * Don't remove the methods appearing as unused, in order not to break the menu animations
  */
 public final class SettingsFrameLayout extends FrameLayout {
+    private float mVisibleness = 1.0f;
 
     public SettingsFrameLayout(Context context) {
         super(context);
@@ -23,5 +25,24 @@ public final class SettingsFrameLayout extends FrameLayout {
 
     public SettingsFrameLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public float getYFraction() {
+        return getY() / getHeight();
+    }
+
+    public void setYFraction(float yFraction) {
+        final int height = getHeight();
+        setY((height > 0) ? (yFraction * height) : -9999);
+    }
+
+    public float getVisibleness() {
+        return mVisibleness;
+    }
+
+    public void setVisibleness(float visibleness) {
+        setScaleX(visibleness);
+        setScaleY(visibleness);
+        setAlpha(visibleness);
     }
 }
