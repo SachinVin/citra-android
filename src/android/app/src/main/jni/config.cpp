844e609f87c094d1bc69c20c36013ff5efe57fc1
FearlessTobi <thm.frey@gmail.com>
2020-04-14 04:00:16 +0200

    android: Fix the frame limiter
    It was erroneously using the wrong config section and variable type.

--- src/android/app/src/main/jni/config.cpp
+++ src/android/app/src/main/jni/config.cpp
@@ -115,7 +115,7 @@ void Config::ReadValues() {
     Settings::values.use_vsync_new = sdl2_config->GetBoolean("Renderer", "use_vsync_new", true);
     Settings::values.use_frame_limit = sdl2_config->GetBoolean("Renderer", "use_frame_limit", true);
     Settings::values.frame_limit =
-        static_cast<u16>(sdl2_config->GetReal("Renderer", "frame_limit", 1.0) * 100.0);
+        static_cast<u16>(sdl2_config->GetInteger("Renderer", "frame_limit", 100));
     Settings::values.texture_filter_name =
             sdl2_config->GetString("Renderer", "texture_filter_name", "none");
 
