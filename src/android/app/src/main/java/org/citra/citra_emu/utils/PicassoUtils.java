5b49201a4add297a23435ab95a7d57dadb3c1e78
zhupengfei <zhupf321@gmail.com>
2020-04-18 20:29:11 +0800

    Address review

--- src/android/app/src/main/java/org/citra/citra_emu/utils/PicassoUtils.java
+++ src/android/app/src/main/java/org/citra/citra_emu/utils/PicassoUtils.java
@@ -8,6 +8,10 @@ import com.squareup.picasso.Picasso;
 
 import org.citra.citra_emu.R;
 
+import java.io.IOException;
+
+import androidx.annotation.Nullable;
+
 public class PicassoUtils {
     public static void loadGameIcon(ImageView imageView, String gamePath) {
         Picasso picassoInstance = new Picasso.Builder(imageView.getContext())
@@ -24,5 +28,20 @@ public class PicassoUtils {
                 .error(R.drawable.no_icon)
                 .transform(new PicassoRoundedCornersTransformation())
                 .into(imageView);
+    }
+
+    // Blocking call. Load image from file and crop/resize it to fit in width x height.
+    @Nullable
+    public static Bitmap LoadBitmapFromFile(String uri, int width, int height) {
+        try {
+            return Picasso.get()
+                    .load(Uri.parse(uri))
+                    .config(Bitmap.Config.ARGB_8888)
+                    .centerCrop()
+                    .resize(width, height)
+                    .get();
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
